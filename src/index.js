7bbad823a2e92a22fb9b4fbbf3c3a05d387ad5db
nanaji0133 <nanajisanka5357@gmail.com>
2020-05-27 12:47:52 -0400

    functional components in seperate folder

--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
+import MyInfo from "./components/MyInfo";
 
-function MyInfo ()
-{
-    return (
-        <div>
-            <h1>Nanaji Sanka</h1>
-            <p> These are my education qualifications</p>
-            <ol>
-                <li>web Developer</li>
-                <li>Masters</li>
-                <li>Btech</li>
-            </ol>
-        </div>
-    );
-}
 
 ReactDom.render(
     <MyInfo />,
