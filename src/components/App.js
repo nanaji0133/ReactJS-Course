b1731dfbc07c3b936b098b8d5e2ec0e113a9eede
nanaji0133 <nanajisanka5357@gmail.com>
2020-05-27 15:48:08 -0400

    JSX to JS, back

--- src/components/App.js
+++ src/components/App.js
@@ -7,9 +7,13 @@ export default App;
 
 function App ()
 {
+    const firstName = "nanaji";
+    const lastName = "sanka";
+    const date = new Date();
     return (
         <div>
             <Header />
+            <h1>{ `${firstName} ${lastName} time is ${date.getDate()} ${date.getMonth()}` }</h1>
             <MainContent />
             <Footer />
         </div>
