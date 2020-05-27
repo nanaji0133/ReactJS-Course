cb32edad48194be750bd49cf03847fdea5af585b
nanaji0133 <nanajisanka5357@gmail.com>
2020-05-27 16:16:33 -0400

    inline style

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,32 @@
 import React from "react";
-import Footer from "./Footer";
-import Header from "./Header";
-import MainContent from "./MainContent";
+// import Footer from "./Footer";
+// import Header from "./Header";
+// import MainContent from "./MainContent";
 export default App;
 
 
 function App ()
 {
-    const firstName = "nanaji";
-    const lastName = "sanka";
-    const date = new Date();
+    const hours = new Date().getHours();
+    console.log(hours);
+    let styles = {
+        margin: 0,
+        height: 100,
+        fontSize: 30,
+        color: "blue",
+        textAlign: "center",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "gray",
+    };
+
+    let day = hours < 12 ? "morning" : hours > 12 && hours < 17 ? "evening" : "night";
+    styles.color = (day === "morning") ? "orange" : "blue";
+
     return (
         <div>
-            <Header />
-            <h1>{ `${firstName} ${lastName} time is ${date.getDate()} ${date.getMonth()}` }</h1>
-            <MainContent />
-            <Footer />
+            <h1 style={ styles }>Good { day }</h1>
         </div>
     );
 }
