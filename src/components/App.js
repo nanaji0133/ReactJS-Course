33bb221c4faadcb920d65c02160016af79be05dc
nanaji0133 <nanajisanka5357@gmail.com>
2020-05-29 11:22:47 -0400

    Class Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,16 @@
 import React from "react";
-import Header from "./Header";
-import todoData from "./todoData";
-import ToDoItem from "./ToDoItem";
+
+
+class App extends React.Component
+{
+    render ()
+    {
+        return (
+            <div>
+                class based react
+            </div>
+        );
+    }
+}
 
 export default App;
-
-function App ()
-{
-    const renderData = todoData.map(data => <ToDoItem key={ data.id }
-        taskData={ data }
-    />);
-    //     taskData={ {
-    //         completed: data.completed,
-    //         task: data.task
-    //     } }
-    // />);
-    return (
-        <div className="my-div">
-            <Header />
-            { renderData }
-        </div>
-    );
-}
