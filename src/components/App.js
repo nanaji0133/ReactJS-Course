d447280ddabd4374c170e0fad77f9d31aa851f49
nanaji0133 <nanajisanka5357@gmail.com>
2020-06-02 15:28:02 -0400

    forms--text

--- src/components/App.js
+++ src/components/App.js
@@ -7,51 +7,33 @@ class App extends Component
     {
         super();
         this.state = {
-            value: "item 1"
+            firstname: "",
+            lastname: "",
         };
 
         this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange (event)
     {
-        this.setState({ value: event.target.value });
-    }
+        this.setState({
 
-    handleSubmit (event)
-    {
-        alert("entered value:" + this.state.value);
-        this.setState({ value: "" });
-        event.preventDefault();
+            [event.target.name]: event.target.value
+        });
     }
 
     render ()
     {
         return (
-            <div className="my-div" onSubmit={ this.handleSubmit }>
-                <form className="my-form" >
-                    {/* <label>
-                        <input type="text" value={ this.state.value }
-                            onChange={ this.handleChange } />
-
-                    </label>
-
-                    <textarea value={ this.state.value }
-                        onChange={ this.handleChange }
-                        style={ { width: "50vw",  } } /> */}
-
-                    <select value={ this.state.value }
-                        onChange={ this.handleChange }>
-                        <option value="item 1">item 1</option>
-                        <option value="item 2">item 2</option>
-                        <option value="item 3">item 3</option>
-                    </select>
-
-                    <label>
-                        <input type="submit" value="submit" />
-                    </label>
-
+            <div className="my-div" >
+                <form>
+                    <input name="firstname" type="text"
+                        placeholder="first name" onChange={ this.handleChange } />
+                    <br />
+                    <input name="lastname" type="text"
+                        placeholder="last name" onChange={ this.handleChange } />
+                    <br />
+                    <h1>{ this.state.firstname } { this.state.lastname }</h1>
                 </form>
             </div>
         );
