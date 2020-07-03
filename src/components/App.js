08c51b817b93ac96128156507953faa406f3efac
nanaji0133 <nanajisanka5357@gmail.com>
2020-07-03 17:19:20 -0400

    REACT DOCUMENTATION

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,39 @@
-import React from "react";
-import TeamContainer from "./TeamContainer";
+import React, { Component } from "react";
 
-function App ()
+
+class App extends Component
 {
-    return (
-        <TeamContainer />
-    );
+    constructor()
+    {
+        super();
+        this.state = {
+            time: new Date().toLocaleTimeString()
+        };
+    }
+
+    componentDidMount ()
+    {
+        this.interval = setInterval(() => this.clock(), 1000);
+    }
+
+    clock ()
+    {
+        this.setState({ time: new Date().toLocaleTimeString() });
+    }
+
+    componentWillMount() {
+        clearInterval(this.interval)
+    }
+
+    render ()
+    {
+        return (
+
+            <div>
+                { this.state.time}
+            </div >
+        );
+    }
 }
 
 export default App;
