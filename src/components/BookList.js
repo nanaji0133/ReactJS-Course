2af3761a4cfba7676ba4601d45cbceb8f9689851
nanaji0133 <nanajisanka5357@gmail.com>
2020-07-08 16:39:43 -0400

    context accessing 2nd way

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -3,20 +3,26 @@ import { ThemeContex } from '../context/ThemeContext';
 
 class BookList extends Component
 {
-    static contextType = ThemeContex;
+
     render ()
     {
-        const { isLight, light, dark } = this.context;
-        const theme = isLight ? light : dark;
         return (
-            <div className="booklist"
-                style={ { backgroundColor: theme.bg, color: theme.syntex } }>
-                <ul>
-                    <li style={ { backgroundColor: theme.ui } }>Book 1</li>
-                    <li style={ { backgroundColor: theme.ui } }>Book 2</li>
-                    <li style={ { backgroundColor: theme.ui } }>Book 3</li>
-                </ul>
-            </div>
+            <ThemeContex.Consumer>{ (context) =>
+            {
+                const { isLight, light, dark } = context;
+                const theme = isLight ? light : dark;
+                return (
+                    <div className="booklist"
+                        style={ { backgroundColor: theme.bg, color: theme.syntex } }>
+                        <ul>
+                            <li style={ { backgroundColor: theme.ui } }>Book 1</li>
+                            <li style={ { backgroundColor: theme.ui } }>Book 2</li>
+                            <li style={ { backgroundColor: theme.ui } }>Book 3</li>
+                        </ul>
+                    </div>
+                );
+            } }
+            </ThemeContex.Consumer>
         );
     }
 }
